2cbf29ccc94420bbedf0c50605bb77f6ba633dc8
cathygutto <32327765+cathygutto@users.noreply.github.com>
2018-05-29 00:19:24 +0200

    meg er konge

--- nettsted/dynamiskeFunksjoner1.php
+++ nettsted/dynamiskeFunksjoner1.php
@@ -1,10 +1,10 @@
 <?php
 
-function listeboksHotellnavn()
+function listeboksHotellnavn($valgtSted)
   {
     include("db-tilkobling.php");
 
-    $sqlSetning="SELECT * FROM hotell ORDER BY hotellnavn;";
+    $sqlSetning="SELECT * FROM hotell WHERE sted='$valgtSted' ORDER BY hotellnavn;";
     $sqlResultat=mysqli_query($db,$sqlSetning) or die ("Ikke mulig &aring; hente data fra db");
 
     $antallRader=mysqli_num_rows($sqlResultat);
@@ -15,7 +15,15 @@ function listeboksHotellnavn()
         $sted=$rad["sted"];
         $hotellnavn=$rad["hotellnavn"];
 
-        print("<option value='$sted', '$hotellnavn'>$hotellnavn</option>");
+        if($sted==$valgtSted)
+					{
+						print("<option value='$sted' selected>$hotellnavn $sted</option>");
+					}
+				else
+					{
+						print("<option value='$sted'>$hotellnavn $sted</option>");
+					}
+        //print("<option value='$sted', '$hotellnavn'>$hotellnavn</option>");
       }
   }
 ?>
